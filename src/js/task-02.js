1f6772581ccec2bc50db633c35b170b3f0df0823
artfull1992 <pavlopetruk92@gmail.com>
2023-02-01 16:47:13 +0200

    task-02-dome

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+ const list = document.querySelector('#ingredients');
+// console.log(list)
+
+
+  const liItems = ingredients.map(element => {
+    const listItem = document.createElement("li")
+    listItem.className = "item"
+    listItem.textContent = element
+    list.append(listItem)
+  });
+
+// console.log(setLiItems(ingredients))
+console.log(liItems)
