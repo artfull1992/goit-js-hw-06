5615ec70837e4971faa3270b364aea9f22d51ede
artfull1992 <pavlopetruk92@gmail.com>
2023-02-01 16:06:56 +0200

    task-01-dome

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,19 @@
-console.log(document.body)
+const getAllLiItem = document.querySelector('#categories')
+
+const getQuantityItemsByUl = (ulItems) =>{
+    console.log('Number of categories: ', ulItems.children.length);
+}
+
+getQuantityItemsByUl(getAllLiItem);
+
+// const liQuantityItems = getAllLiItem.querySelectorAll('.item')
+
+const getLiAndH2ByLiItems = (liItems) => {
+    const liItemsArray = liItems.querySelectorAll('.item')
+    for (const element of liItemsArray) {
+       console.log('Category: ',element.querySelector('h2').textContent);
+       console.log('Elements: ',element.querySelectorAll('li').length)
+    }
+}
+
+getLiAndH2ByLiItems(getAllLiItem);
